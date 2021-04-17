9c7202201d07ca2865c1496d0eba14cc4e76ebae
Carla G. Chiodi <carla.chiodi@gmail.com>
2021-04-18 00:17:53 +0100

    Styles to categories

--- src/Category.js
+++ src/Category.js
@@ -5,7 +5,6 @@ export default function Category(props) {
   return (
     <div className="category">
       <img src={props.image} alt={props.type} className="category-img" />
-      <br />
       <strong>{props.type}</strong>
     </div>
   )
